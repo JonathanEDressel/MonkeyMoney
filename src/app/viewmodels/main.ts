ef2153338ad8ac7ddc669ed4226bee9c4bf7e2b3
Jonathan Dressel <jonathanedressel@gmail.com>
2025-10-04 17:02:34 -0700

    bound the pages in main

--- src/app/viewmodels/main.ts
+++ src/app/viewmodels/main.ts
@@ -3,10 +3,12 @@ import { Component } from '@angular/core';
 import { UserData } from '../services/userdata';
 import { UserModel } from '../models/usermodel';
 import { OverviewComponent } from './portal/overview';
+import { from, of, Observable, Subscriber } from 'rxjs';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'main-root',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink],
   templateUrl: '../views/main.html',
   styleUrl: '../styles/main.scss'
 })
@@ -15,16 +17,27 @@ export class MainComponent {
     
     selectedPage: number = 1;
     portalPages = [
-      { Id: 1, Title: 'Overview', Route: '/main', Visible: true, View: OverviewComponent },
-      { Id: 2, Title: 'History', Route: '/main', Visible: true, View: '' },
-      { Id: 3, Title: 'Account', Route: '/main', Visible: true, View: '' },
-      { Id: 4, Title: 'Profile', Route: '/main', Visible: true, View: '' }
+      { Id: 1, Title: 'Overview', Route: '/main', Visible: true, isSelected: Observable<false>, View: OverviewComponent },
+      { Id: 2, Title: 'History', Route: '/main', Visible: true, isSelected: Observable<false>, View: '' },
+      { Id: 3, Title: 'Account', Route: '/main', Visible: true, isSelected: Observable<false>, View: '' },
+      { Id: 4, Title: 'Profile', Route: '/main', Visible: true, isSelected: Observable<false>, View: '' }
     ];
 
     constructor(private usrData: UserData) {}
     
+    currentPage: Observable<any> = from(this.portalPages);
+
+    setPage(page: any) {
+      this.selectedPage = page.Id;
+      console.log(page.Title);
+    }
+
     ngOnInit(): void {
       this.activate();
+
+      this.currentPage.subscribe(x => {
+        console.log(x.Title)
+      });
     }
 
     activate() {
