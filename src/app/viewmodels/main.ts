41e77e74d62e545fb8015790c65b59858de34fbf
Jonathan Dressel <jonathanedressel@gmail.com>
2025-10-04 17:34:54 -0700

    show pages when nav is clicked

--- src/app/viewmodels/main.ts
+++ src/app/viewmodels/main.ts
@@ -3,12 +3,13 @@ import { Component } from '@angular/core';
 import { UserData } from '../services/userdata';
 import { UserModel } from '../models/usermodel';
 import { OverviewComponent } from './portal/overview';
-import { from, of, Observable, Subscriber } from 'rxjs';
+import { Observable } from 'rxjs';
 import { RouterLink } from "@angular/router";
+import { NgComponentOutlet } from '@angular/common';
 
 @Component({
   selector: 'main-root',
-  imports: [FormsModule, RouterLink],
+  imports: [FormsModule, RouterLink, NgComponentOutlet],
   templateUrl: '../views/main.html',
   styleUrl: '../styles/main.scss'
 })
@@ -18,14 +19,17 @@ export class MainComponent {
     selectedPage: number = 1;
     portalPages = [
       { Id: 1, Title: 'Overview', Route: '/main', Visible: true, isSelected: Observable<false>, View: OverviewComponent },
-      { Id: 2, Title: 'History', Route: '/main', Visible: true, isSelected: Observable<false>, View: '' },
-      { Id: 3, Title: 'Account', Route: '/main', Visible: true, isSelected: Observable<false>, View: '' },
-      { Id: 4, Title: 'Profile', Route: '/main', Visible: true, isSelected: Observable<false>, View: '' }
+      { Id: 2, Title: 'History', Route: '/main', Visible: true, isSelected: Observable<false>, View: null },
+      { Id: 3, Title: 'Account', Route: '/main', Visible: true, isSelected: Observable<false>, View: null },
+      { Id: 4, Title: 'Profile', Route: '/main', Visible: true, isSelected: Observable<false>, View: null }
     ];
 
     constructor(private usrData: UserData) {}
     
-    currentPage: Observable<any> = from(this.portalPages);
+    get getPage() {
+      var page = this.portalPages.find(pg => this.selectedPage === pg.Id);
+      return page?.View ?? null;
+    }    
 
     setPage(page: any) {
       this.selectedPage = page.Id;
@@ -33,22 +37,9 @@ export class MainComponent {
     }
 
     ngOnInit(): void {
-      this.activate();
-
-      this.currentPage.subscribe(x => {
-        console.log(x.Title)
-      });
     }
 
     activate() {
-      this.usrData.getUsers().subscribe({
-        next: (dta) => {
-          this.users = dta;
-          console.log('users - ', this.users);
-        },
-        error: (err) => {
-          console.log('ERROR: ', err); 
-        }
-      })
+
     }
 }
